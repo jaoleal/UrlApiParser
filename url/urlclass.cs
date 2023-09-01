30c92d89dd79c4117e9a0df7dc2e7e72b07cab73
JoaoLeal <devjoaoleal1006@gmail.com>
2023-09-01 11:21:12 -0300

    added url generic class

---
+++ url/urlclass.cs
@@ -0,0 +1,17 @@
+public class Url{
+    public string scheme;
+    public string domain;
+    public string subdomain;
+    public string path;
+    public string topleveldomain;
+    public string querystring;
+
+    public Url(string _scheme _domain _subdomain _path _toplevedomain _querystring){
+        scheme = _scheme;
+        domain = _domain;
+        subdomain = _subdomain;
+        path = _path;
+        topleveldomain = _toplevedomain
+        querystring = _querystring;
+    }
+}
