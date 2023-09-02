9679fb2e30c1a091f1fb0ab193fad9791b5c8bb9
JoaoLeal <devjoaoleal1006@gmail.com>
2023-09-02 18:04:50 -0300

    changed structure and added youtube child class

--- url/urlclass.cs
+++ url/urlclass.cs
@@ -1,21 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace classes{
     public class URL{
-        public string ?scheme;
-        public string ?domain;
-        public string ?subdomain;
-        public string ?path;
-        public string ?topleveldomain;
-        public string ?querystring;
-        public string[] allmodules;
+        protected string? scheme{ get; set; }
+        protected string? domain{ get; set; }
+        protected string? subdomain{ get; set; }
+        protected string? path{ get; set; }
+        protected string? topleveldomain{ get; set; }
+        protected string[]? allmodules{ get; set; }
 
-        public URL(string _scheme, string _domain, string _subdomain, string _path, string _toplevedomain, string _querystring){
-            scheme = _scheme;
-            domain = _domain;
-            subdomain = _subdomain;
-            path = _path;
-            topleveldomain = _toplevedomain;
-            querystring = _querystring;
-            allmodules = new string[] {scheme, domain, subdomain, path, topleveldomain,querystring};
-        }
+        public void parserinput(string[] modules){
+            this.scheme = modules[0];
+            this.domain = modules[1];
+            this.topleveldomain = modules[2] + "." + modules[3];
+            this.path = modules[4];
+
+            this.allmodules = modules;
+
+        } 
+}
+    
+    public class YoutubeURL: URL{
+
+
+        public string VideoId{ get; set; }
+        public string? Channel{ get; set; }
+        public string? Seconds{ get; set; }
+            
     }
 }
