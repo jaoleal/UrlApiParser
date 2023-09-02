7458baf02b84c8094b5b7a2170d432cbde3bcb85
JoaoLeal <devjoaoleal1006@gmail.com>
2023-09-02 18:05:43 -0300

    new logic for simpleest code

--- url/core.cs
+++ url/core.cs
@@ -1,69 +1,32 @@
-﻿using classes;
+﻿using System;
+using System.Security.AccessControl;
+using classes;
 namespace core{
 public class CoreParseFunc{
-        public URL[] URLObjects= new URL[1];
         public CoreParseFunc(string url ){
             
         }
-        public CoreParseFunc(string[] urls){
+        public CoreParseFunc(string[] urls, URL[] objects){
 
-            static string ParseUrl(string url, string target){   
+            static URL ParseUrl(string url, URL obj){
                 
                 //divisors group that separate strings.
                 string[] Divisors = {"Null"};
 
-                //index of the target in the output.
-                int moduleindex = 0;
+                //index of the modules
+                //scheme = 0
+                //domain = 1
+                //topleveldomain = 2 if has two components + "." + 3
+                //path is the rest or 4 
 
-                switch(target){
-                    case "Null":
-                    Console.WriteLine("Divisores não configurados corretamente");
-                    break;
-
-                    case "domain":
-                        Divisors = new string[] {"https://www.", ".com",};
-                        moduleindex = 0;
-                    break;
-
-                    case "scheme":
-                        Divisors = new string[] {"."};
-                        moduleindex = 0;
-                    break;
-                    case "topleveldomain":
-                        Divisors = new string[] {"https://www","."};
-                        moduleindex = 1; 
-                    break;
-                    case "path":
-                        Divisors = new string[] {"https://wwww.", ".com", "/"};
-                        moduleindex = 1;
-                    break;
-                    case "querystring":
-                        Divisors = new string[] {"?"};
-                        moduleindex = 1;
-                    break;
-                    default:
-                        return "Divisor not recognized";
-                    break;
-
-                }
+                
                 string[] cache = url.Split(Divisors, StringSplitOptions.RemoveEmptyEntries);
                 string[] urlmodules = cache;
-                if(moduleindex+1 > urlmodules.Length){
-                    return "null";
-                }else{
-                     return urlmodules[moduleindex];
-                }
+                obj.parserinput(urlmodules);
+                return obj;
+                
                
-            }
-            URLObjects = new URL[urls.Length];
-            int i = 0;
-            foreach(string url in urls){
-                URLObjects[i] = new URL(ParseUrl(url, "scheme"),ParseUrl(url, "domain"),ParseUrl(url, "subdomain"),ParseUrl(url, "path"),ParseUrl(url, "topleveldomain"),ParseUrl(url, "querystring"));
-
-                i++;
-            }
-
-        
+            }        
         }
 }
 }
